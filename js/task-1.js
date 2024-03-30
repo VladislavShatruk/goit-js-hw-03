b4346844ae77e8322e599d81cdf18d79339efa5e
VladislavShatruk <vladshatruk121@gmail.com>
2024-03-30 17:34:00 +0200

    goit-js-hw-03

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,14 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    const totalPrice = quantity * pricePerDroid;
-    
-    if (customerCredits < totalPrice) {
-       return "Insufficient funds!";
-    }
-     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+console.log("task-1");
+
+function slugify(title){
+   const normTitle = title.toLowerCase();
+   const slug = normTitle.split(" ");
+   return slug.join("-")
 }
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
